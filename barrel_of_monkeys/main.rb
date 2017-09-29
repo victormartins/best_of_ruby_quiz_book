b43bd8edcd1f413ff1d7ad5ddd3a0a49bb53f15f
Victor Martins <victor.martins@sage.com>
2017-09-29 23:31:37 +0100

    first ordering examples

--- barrel_of_monkeys/main.rb
+++ barrel_of_monkeys/main.rb
@@ -4,14 +4,39 @@ class Main
   end
 
   def playlist(first_song_name: nil)
-    set_first_song(first_song_name) if first_song_name
-    @memory
+    return @memory if @memory.empty?
+
+    @first_song = if first_song_name
+      extract_first_song(first_song_name)
+    else
+      @memory[0]
+    end
+
+    order_by_last_song_letter
+    [@first_song] + @memory
   end
 
   private
 
-  def set_first_song(first_song_name)
-    song = @memory.delete(first_song_name)
-    @memory = [song] + @memory
+  def extract_first_song(first_song_name)
+    @memory.delete(first_song_name)
+  end
+
+  def order_by_last_song_letter
+    result = []
+    first_iteration = true
+
+    while(@memory.any?) do
+      @first_song = result[-1] unless first_iteration
+      match_song = @memory.select { |song| song[0].downcase == @first_song[-1].downcase  }
+      result << @memory.slice!(@memory.find_index(match_song[0]))
+
+      if @memory.length == 1
+        result << @memory.pop
+      end
+      first_iteration = false
+    end
+
+    @memory = result.flatten
   end
 end
--- barrel_of_monkeys/spec/main_spec.rb
+++ barrel_of_monkeys/spec/main_spec.rb
@@ -13,20 +13,41 @@ RSpec.describe Main do
       expect(subject.playlist.object_id).to_not eq(library.object_id)
     end
 
-    context 'when I pass a library songs' do
-      let(:library) { ['z','Klucks','Focks','Suck'] }
+    context 'when I pass a library of songs' do
+      let(:library) { ['K1luckf','F2ocks','S3uck'] }
 
       it 'contains the songs of that library' do
-        expect(subject.playlist).to include('Klucks')
+        expect(subject.playlist).to include('K1luckf')
       end
 
       it 'allows us to define the name of the first song' do
-        expect(subject.playlist(first_song_name: 'Focks')).to start_with('Focks')
+        expect(subject.playlist(first_song_name: 'F2ocks')).to start_with('F2ocks')
       end
 
-      xit 'returns songs ordered by the last letter' do
-        expect(subject.playlist).to be(['Focks', 'Suck', 'Klucks'])
+      it 'returns songs ordered by the last letter' do
+        expect(subject.playlist(first_song_name: 'F2ocks')).to eq(['F2ocks', 'S3uck', 'K1luckf'])
       end
+    end
+
+    context 'when passing two ordered songs' do
+      let(:library) { ['az','za'] }
+
+      it 'returns only two songs' do
+        pending('There is a bug where we get a copy of a song.')
+        expect(subject.playlist).to eq(['az','za'])
+      end
+    end
+
+    context 'when passing two unordered songs' do
+      let(:library) { ['az','za'] }
+
+      it 'returns only two songs' do
+        expect(subject.playlist(first_song_name: 'za')).to eq(['za', 'az'])
+      end
+    end
+
+    context 'when the first song does not relate no any other song' do
+      it '____DEFINE_BEHAVIOUR___'
     end
   end
 end
