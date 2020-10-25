806611910ac5b16a3309fca8f38f658d3de5df90
Victor Guiomar Martins <correio@victormartins.com>
2020-10-25 11:44:33 +0000

    Move logic about reading a file into main class.

--- 03__gedcom_parser/lib/gedcom_parser.rb
+++ 03__gedcom_parser/lib/gedcom_parser.rb
@@ -1,8 +1,27 @@
 require 'xml_creator'
 
 class GedcomParser
-  def call(input:)
+  class Error < StandardError; end
+
+  def call(input_file:)
+    assert_file_exists!(input_file)
+
+    input = File.readlines(input_file)
+
     xml = XMLCreator.new(root: 'gedcom')
+
+    input.each do |line|
+
+    end
+
     xml.call
+  end
+
+  private
+
+  def assert_file_exists!(file_input)
+    return true if File.exist?(file_input)
+
+    raise(Error, "File not found: #{file_input}")
   end
 end
--- 03__gedcom_parser/spec/gedcom_parser_spec.rb
+++ 03__gedcom_parser/spec/gedcom_parser_spec.rb
@@ -1,10 +1,20 @@
 require 'gedcom_parser'
 
 RSpec.describe GedcomParser do
-  let(:small_sample_gemdcom) { File.read("#{__dir__}/sample_data/small_sample.gedcom") }
+  let(:small_sample_gemdcom_path) {"#{__dir__}/sample_data/small_sample.gedcom" }
   let(:small_sample_xml) { File.read("#{__dir__}/sample_data/small_sample.xml") }
+
   it 'Parses gedcom data correctly' do
-    result = subject.call(input: small_sample_gemdcom)
+    result = subject.call(input_file: small_sample_gemdcom_path)
     expect(result).to eql(small_sample_xml)
+  end
+
+  context 'When the file does not exist' do
+    it 'raises an exception' do
+      expect { subject.call(input_file: 'will_fail') }.to raise_error(
+        GedcomParser::Error,
+        'File not found: will_fail'
+      )
+    end
   end
 end
